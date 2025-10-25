f2b9137e8ed58f210ec978d828075759323b35d4
Egor <egor_udir@mail.ru>
2025-10-25 09:22:51 +0300

    Добавлен шаблон задания

---
+++ src/PracticalWork.Library.Data.PostgreSql/Configurations/FictionBookConfiguration.cs
@@ -0,0 +1,5 @@
+﻿using PracticalWork.Library.Data.PostgreSql.Entities;
+
+namespace PracticalWork.Library.Data.PostgreSql.Configurations;
+
+internal sealed class FictionBookConfiguration : EntityConfigurationBase<FictionBookEntity>;
