b973f7204099aa800073a0f3fe6bef00b566d4c1
Egor <egor_udir@mail.ru>
2026-05-16 20:22:04 +0300

    В пакет межсервисных контрактов добавлен контракт для генерации еженедельного отчета

---
+++ PracticalWork.Shared/src/PracticalWork.Shared.Contracts/Http/Reports/WeeklyReport/GenerateWeeklyReportRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticalWork.Shared.Contracts.Http.Reports.WeeklyReport;
+
+/// <summary>
+/// Запрос на генерацию отчета с еженедельной статистикой библиотеки
+/// </summary>
+/// <param name="PeriodFrom">Начало периода отчетности (формат ввода: yyyy-mm-dd)</param>
+/// <param name="PeriodTo">Конец периода отчетности (формат ввода: yyyy-mm-dd)</param>
+/// <param name="NewBooksCount">Количество новых книг за период</param>
+/// <param name="NewReadersCount">Количество новых читателей за период</param>
+/// <param name="BorrowedBooksCount">Количество выданных книг за период</param>
+/// <param name="ReturnedBooksCount">Количество возвращенных книг за период</param>
+/// <param name="OverdueBooksCount">Количество просроченных выдач на конец периода</param>
+public sealed record GenerateWeeklyReportRequest(
+    DateOnly PeriodFrom,
+    DateOnly PeriodTo,
+    int NewBooksCount,
+    int NewReadersCount,
+    int BorrowedBooksCount,
+    int ReturnedBooksCount,
+    int OverdueBooksCount);
---
+++ PracticalWork.Shared/src/PracticalWork.Shared.Contracts/Http/Reports/WeeklyReport/GenerateWeeklyReportResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticalWork.Shared.Contracts.Http.Reports.WeeklyReport;
+
+/// <summary>
+/// Ответ на запрос о генерации отчета с еженедельной статистикой библиотеки
+/// </summary>
+/// <param name="Name">Название отчета</param>
+/// <param name="DownloadUrl">URL для скачивания</param>
+/// <param name="GeneratedAt">Дата и время генерации</param>
+public sealed record GenerateWeeklyReportResponse(
+    string Name,
+    string DownloadUrl,
+    DateTime GeneratedAt);
